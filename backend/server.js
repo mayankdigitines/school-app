d5a480f551c22763b3508cb84aa4c736e221365f
mayank <mayankmayank1225@gmail.com>
2026-02-06 01:06:18 +0530

    refactor: reorder imports for better organization

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config({quiet: true});
 
-import app from './app.js';
+
 import connectDB from './config/db.js';
 
 // Connect to Database
 connectDB();
+
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
